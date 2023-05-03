15f90d4c859045b3740682d32f080a7810cf103e
Juan Diego Guerra <jdxelric@gmail.com>
2023-05-02 23:57:16 -0500

    Game Logic / Variables

--- js/game_script.js
+++ js/game_script.js
@@ -22,7 +22,56 @@ function Loop() {
     requestAnimationFrame(Loop);
 }
 
+//****** GAME LOGIC ********//
 
+var nivelDelMar = 100;
+var nivelDelMarCubriendo = 60;
+var velY = 0;
+var impulso = 900;
+var impulsoEnAgua = 500;
+var gravedad = 2500;
+var densidad = 0.00035;
+var coeficienteRozamiento = 0.01;
+
+var dinoPosX = 42;
+var dinoPosY = nivelDelMar; 
+
+var sueloX = 0;
+var velEscenario = 1280/3;
+var gameVel = 1;
+var score = 0;
+
+var parado = false;
+var saltando = false;
+
+var tiempoHastaMoneda = 2;
+var tiempoMonedaMin = 0.3;
+var tiempoMonedaMax = 1.8;
+var monedaMinY = 5;
+var monedaMaxY = 320;
+
+var tiempoHastaObstaculo = 2;
+var tiempoObstaculoMin = 0.7;
+var tiempoObstaculoMax = 1.8;
+
+var interactuables = [];
+
+var tiempoHastaNube = 0.5;
+var tiempoNubeMin = 0.7;
+var tiempoNubeMax = 2.7;
+var maxNubeY = 320;
+var minNubeY = 160;
+var nubes = [];
+var velNube = 0.5;
+
+var contenedor;
+var dino;
+var textoScore;
+var suelo;
+var gameOver;
+var audioMoneda;
+var audioSalto;
+var audioGameOver;
 
 
 
